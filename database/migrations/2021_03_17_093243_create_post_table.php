94dd5b406f9041c2c4b7b7d5963730d9b3aa95d7
bill <kousikac06@gmail.com>
2021-04-24 23:16:55 +0800

    1.調整migration名稱,users/posts改為user/post 2.新增user查詢索引

--- database/migrations/2021_03_17_093243_create_post_table.php
+++ database/migrations/2021_03_17_093243_create_post_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreatePostTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('post', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->longText('content');
             $table->string('category');
             $table->timestamp('published_at');
             $table->timestamps();
+
+            $table->index(['user_id', 'category', 'published_at'], 'search_index');
         });
     }
 
@@ -30,6 +32,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('post');
     }
 }
