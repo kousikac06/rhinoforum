782ae8434078dfe789c16f7c14d26049aba99998
Kay <kousikac06@gmail.com>
2021-08-25 14:12:53 +0800

    dev step 1

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,7 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     /**
-     * 取得分頁參數22
+     * 取得分頁參數55
      *
      * @param $formData
      * @return mixed
